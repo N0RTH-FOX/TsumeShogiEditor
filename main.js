07eaa292903b75091e0ea0d804cb0c77d78be6f8
きたきつね｡ <orangetanto1009@gmail.com>
2026-04-13 17:06:38 +0900

    まずはね、

---
+++ main.js
@@ -0,0 +1,27 @@
+let element_hu_stok = document.getElementById('piece_hu_stok');
+let element_king_stok = document.getElementById('piece_king_stok');
+let element_kin_stok = document.getElementById('piece_kin_stok');
+let element_gin_stok = document.getElementById('piece_gin_stok');
+let element_kei_stok = document.getElementById('piece_kei_stok');
+let element_kyou_stok = document.getElementById('piece_kyou_stok');
+let element_hi_stok = document.getElementById('piece_hi_stok');
+let element_kaku_stok = document.getElementById('piece_kaku_stok');
+let piece_hu_stok = 9;
+let piece_king_stok = 1;
+let piece_kin_stok = 2;
+let piece_gin_stok = 2;
+let piece_kei_stok = 2;
+let piece_kyou_stok = 2;
+let piece_hi_stok = 1;
+let piece_kaku_stok = 1;
+
+
+
+element_hu_stok.textContent = piece_hu_stok;
+element_king_stok.textContent = piece_king_stok;
+element_kin_stok.textContent = piece_kin_stok;
+element_gin_stok.textContent = piece_gin_stok;
+element_kei_stok.textContent = piece_kei_stok;
+element_kyou_stok.textContent = piece_kyou_stok;
+element_hi_stok.textContent = piece_hi_stok;
+element_kaku_stok.textContent = piece_kaku_stok;
